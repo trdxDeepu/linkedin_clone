ba3b55c7d081d86dd193c506f259a2d30a5e956d
trdxlucifer <singhdepu566@gmail.com>
2023-04-01 15:58:32 +0530

    initialize the firebase and install the firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-    Linkedin Clone
+      <h1>Linkedin CLone</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDFRxviPnNtxx6GYN_b00XnpIFW_Eq2gTs",
+  authDomain: "linkedin-clone-79233.firebaseapp.com",
+  projectId: "linkedin-clone-79233",
+  storageBucket: "linkedin-clone-79233.appspot.com",
+  messagingSenderId: "807489038666",
+  appId: "1:807489038666:web:27f7f68f2ed3306c0b39b3",
+};
+
+const app = initializeApp(firebaseConfig);
