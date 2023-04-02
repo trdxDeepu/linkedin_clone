7f676e6a3aed6193e960d87282a4647b456330ea
trdxlucifer <singhdepu566@gmail.com>
2023-04-02 13:02:33 +0530

    chnages in css and added input to login

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Approuter from "./Routes/Approuter";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app" >
      <Approuter/>
 
     </div>
--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import { RegisterApi,LoginApi } from "../api/AuthApi";
+import { RegisterApi, LoginApi } from "../api/AuthApi";
 import "../sass/LoginComponent.scss";
-import Logo from '../assets/linkedin-logo.png'
+import Logo from "../assets/linkedin-logo.png";
 
 const LoginComponent = () => {
   const [data, setData] = useState({
@@ -18,38 +18,38 @@ const LoginComponent = () => {
     });
   };
 
-const login = async () => {
-
-try {
-  await LoginApi(data.email , data.password)
-} catch (error) {
-  
-}
-
-}
+  const login = async () => {
+    try {
+      await LoginApi(data.email, data.password);
+    } catch (error) {}
+  };
   return (
     <div className="login-wrapper">
-      <h1 className="heading">Sign in</h1>
       <img src={Logo} alt="logo" className="linkedin-logo" />
-      <input
-        type="text"
-        placeholder="Enter email"
-        className="common-input"
-        value={email}
-        id="email"
-        onChange={handleChange}
-      />
-      <input
-        type="password"
-        placeholder="Enter password"
-        className="common-input"
-        value={password}
-        id="password"
-        onChange={handleChange}
-      />
-      <button onClick={login} className="login-btn">
-        Login in linkedin
-      </button>
+      <h1 className="heading">Sign in</h1>
+      <p className="sub-heading">Stay up to date on your professional world</p>
+
+      <div className="auth-input">
+        <input
+          type="text"
+          placeholder="Enter email"
+          className="common-input"
+          value={email}
+          id="email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Enter password"
+          className="common-input"
+          value={password}
+          id="password"
+          onChange={handleChange}
+        />
+        <button onClick={login} className="login-btn">
+          Login in linkedin
+        </button>
+      </div>
     </div>
   );
 };
