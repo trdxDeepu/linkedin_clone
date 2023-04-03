90a665294f642b90e4b8682487af350fb503f91c
trdxlucifer <singhdepu566@gmail.com>
2023-04-03 20:50:51 +0530

    added navigate

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,10 @@ import HomeComponent from "../components/HomeComponent";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../Firebase";
 import Loader from "../common/Loader/Loader";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate()
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
