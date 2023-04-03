4c0f609b9eee666714e2e0a8dd189d1d29835e52
trdxlucifer <singhdepu566@gmail.com>
2023-04-03 13:49:49 +0530

    added react icons library and created top bar compnent using react icons

--- src/common/topbar/Topbar.jsx
+++ src/common/topbar/Topbar.jsx
@@ -6,22 +6,25 @@ import {
   AiOutlineUserSwitch,
   AiOutlineSearch,
   AiOutlineMessage,
-  AiOutlineBell
+  AiOutlineBell,
 } from "react-icons/ai";
 import { BsBriefcase } from "react-icons/bs";
+import User from "../../assets/user.png";
 
 const Topbar = () => {
   return (
     <div className="topbar-main">
       <img src={Linkedin} alt="logo" className="linkedin-logo" />
       <div className="topbar-icons">
-        <AiOutlineSearch size={25} className="react-icons"/>
-        <AiOutlineHome size={30}  className="react-icons"/>
+        <AiOutlineSearch size={25} className="react-icons" />
+        <AiOutlineHome size={30} className="react-icons" />
         <AiOutlineUserSwitch size={30} className="react-icons" />
         <BsBriefcase size={30} className="react-icons" />
-        <AiOutlineMessage size={30} className="react-icons"/>
-        <AiOutlineBell size={30} className="react-icons"/>
+        <AiOutlineMessage size={30} className="react-icons" />
+        <AiOutlineBell size={30} className="react-icons" />
       </div>
+
+      <img src={User} alt="userIcon" className="user-logo" />
     </div>
   );
 };
