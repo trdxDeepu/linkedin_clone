b72cd617ef02a1c88b195c99506195ba1515282a
trdxlucifer <singhdepu566@gmail.com>
2023-04-04 12:57:59 +0530

    created modal and in post update  pass the status and and made some chnages in modal if we dont have text so the post button is disable or if text then enables

--- src/components/common/Modal/PostModal.jsx
+++ src/components/common/Modal/PostModal.jsx
@@ -1,26 +1,36 @@
+import { useState } from "react";
+import { Button, Modal } from "antd";
+import "./postmodal.scss";
 
-import { Button, Modal } from 'antd';
-import './postmodal.scss'
-
-const PostModal= ({modalopen,setModalOpen}) => {
-
-
+const PostModal = ({ modalopen, setModalOpen, setStatus, status }) => {
   return (
     <>
-     
       {/* <Button type="primary" onClick={() => setModalOpen(true)}>
         Vertically centered modal dialog
       </Button> */}
       <Modal
-        title="Vertically centered modal dialog"
+        title="Create a Post"
         centered
         open={modalopen}
         onOk={() => setModalOpen(false)}
         onCancel={() => setModalOpen(false)}
+        footer={[
+          <Button
+            key="submit"
+            type="primary"
+            disabled={status.length > 0 ? false : true}
+          >
+            Post
+          </Button>,
+        ]}
       >
-        <p>some contents...</p>
-        <p>some contents...</p>
-        <p>some contents...</p>
+        <input
+          type="text"
+          className="modal-input"
+          placeholder="What do u want to talk about?"
+          value={status}
+          onChange={(event) => setStatus(event.target.value)}
+        />
       </Modal>
     </>
   );
--- src/components/common/PostUpdate/Postupdate.jsx
+++ src/components/common/PostUpdate/Postupdate.jsx
@@ -1,18 +1,26 @@
-import React, { useState } from 'react';
-import './postupdate.scss';
-import PostModal from '../Modal/PostModal';
+import React, { useState } from "react";
+import "./postupdate.scss";
+import PostModal from "../Modal/PostModal";
 
 const Postupdate = () => {
   const [modalOpen, setModalOpen] = useState(false);
+  const [status, setStatus] = useState("");
 
   return (
-    <div className='post-status-main'>
-        <div className='post-status'>
-          <button className='open-post-modal' onClick={() => setModalOpen(true)}>Start a post</button>
-        </div>
-        <PostModal modalopen={modalOpen} setModalOpen={setModalOpen}/>
+    <div className="post-status-main">
+      <div className="post-status">
+        <button className="open-post-modal" onClick={() => setModalOpen(true)}>
+          Start a post
+        </button>
+      </div>
+      <PostModal
+        status={status}
+        setStatus={setStatus}
+        modalopen={modalOpen}
+        setModalOpen={setModalOpen}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Postupdate
+export default Postupdate;
