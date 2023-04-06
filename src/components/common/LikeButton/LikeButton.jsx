4c95bfec2aa9f195531d645e896f0d339bcf7a9d
trdxlucifer <singhdepu566@gmail.com>
2023-04-06 19:03:57 +0530

    added onclick on comment

--- src/components/common/LikeButton/LikeButton.jsx
+++ src/components/common/LikeButton/LikeButton.jsx
@@ -12,31 +12,93 @@ import { likePost, getLikesByUser } from "../../../api/FirestoreApi";
 const LikeButton = ({ userId, postId }) => {
   const [likesCount, setLikesCount] = useState(0);
   const [liked, setLiked] = useState(false);
+  const [showCommentBox, setShowCommentBox] = useState(false);
 
+  const [comment, setComment] = useState("");
+  const [comments, setComments] = useState([]);
+  
   const handleLike = () => {
     likePost(userId, postId, liked);
   };
-
+  
+  const getComment = (event) => {
+    setComment(event.target.value);
+  };
   console.log(liked);
+
+  const addComment = () =>{
+    
+  }
+
 
   useMemo(() => {
     getLikesByUser(userId, postId, setLiked, setLikesCount);
   }, [userId, postId]);
 
   return (
-    <div className="like-container" onClick={handleLike}>
-      <p>{likesCount} people like the post</p>
-   <div className="hr-line">
-    <hr />
-   </div>
-      <div className="likes-inner">
-        {liked ? (
-          <AiFillHeart size={30} color="#0a66c2" />
-        ) : (
-          <AiOutlineHeart size={30} />
-        )}
-        <p className={liked?'blue':'black'}>Like</p>
+    <div className="like-container">
+      <p>{likesCount} People Like this Post</p>
+      <div className="hr-line">
+        <hr />
       </div>
+      <div className="like-comment">
+        <div className="likes-comment-inner" onClick={handleLike}>
+          {liked ? (
+            <BsFillHandThumbsUpFill size={30} color="#0a66c2" />
+          ) : (
+            <BsHandThumbsUp size={30} />
+          )}
+
+          <p className={liked ? "blue" : "black"}>Like</p>
+        </div>
+        <div
+          className="likes-comment-inner"
+          onClick={() => setShowCommentBox(!showCommentBox)}
+        >
+          {
+            <AiOutlineComment
+              size={30}
+              color={showCommentBox ? "#0a66c2" : "#212121"}
+            />
+          }
+
+          <p className={showCommentBox ? "blue" : "black"}>Comments</p>
+        </div>
+      </div>
+      {showCommentBox ? (
+        <>
+          <input
+            onChange={getComment}
+            placeholder="Add a Comment"
+            className="comment-input"
+            name="comment"
+            value={comment}
+          />
+          <button className="add-comment-btn" onClick={addComment}>
+            Add Comment
+          </button>
+
+          {comments.length > 0 ? (
+            comments.map((comment) => {
+              return (
+                <div className="all-comments">
+                  <p className="name">{comment.name}</p>
+                  <p className="comment">{comment.comment}</p>
+
+                  <p className="timestamp">{comment.timeStamp}</p>
+                  {/* 
+                  <p>•</p>
+                   */}
+                </div>
+              );
+            })
+          ) : (
+            <></>
+          )}
+        </>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
