c07a688d6bec35575033f4d39d603460a7aedfb2
trdxlucifer <singhdepu566@gmail.com>
2023-04-07 13:15:27 +0530

    crrated a editt and delette button on post and modal

--- src/components/common/PostUpdate/Postupdate.jsx
+++ src/components/common/PostUpdate/Postupdate.jsx
@@ -31,7 +31,7 @@ const Postupdate = ({ currentUser }) => {
   const getEditData = (posts) => {
     setModalOpen(true);
     setStatus(posts?.status);
-    setCurrentPost(posts);
+    // setCurrentPost(posts);
     setItEdit(true);
   };
 
