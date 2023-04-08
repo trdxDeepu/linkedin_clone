a5470535b984eb083f122083e605bee03f0cd05b
trdxlucifer <singhdepu566@gmail.com>
2023-04-08 19:30:55 +0530

    install reactquills

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import Approuter from "./Routes/Approuter";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import 'react-quill/dist/quill.snow.css';
+
+
 function App() {
   return (
     <div className="app">
